15fbd0b3f8da3f4f864f6dacb64daec435a2e93e
Nicholas Boren <nboren@cs.washington.edu>
2022-05-22 23:14:09 -0700

    fixed: Summary scripts reading incorrectly

--- summary_information_script.R
+++ summary_information_script.R
@@ -1,9 +1,9 @@
 library(dplyr)
 library(stringr)
 
-data1 <- read.csv("nba3_2021_22_corrections.csv")
-data2 <- read.csv2("NBA_2018_per_poss.csv")
-data3 <- read.csv2("2021-2022 NBA Player Stats - Playoffs.csv")
+data1 <- read.csv("data/nba3_2021_22_corrections.csv")
+data2 <- read.csv("data/NBA_2018_per_poss.csv")
+data3 <- read.csv("data/2021-2022 NBA Player Stats - Playoffz.csv")
 
 # Creates list of dataset information
 summary_info <- list()
